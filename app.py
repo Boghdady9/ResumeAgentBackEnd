42c5f829359ab5e2a77c649cc01a730d26560a5d
WarrenGSlice <WarrenPeterson141414@Gmail.com>
2024-11-30 14:45:16 -0800

    attempt to rebase

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from data_loader import PDFTextProcessor
 from Agent import agent as create_agent
 
 # Load a resume PDF file
-processer=PDFTextProcessor("MohamedAbdalla_BEF.pdf")
+processer=PDFTextProcessor("pmres/Alexander_Mejia_Resume.pdf")
 cv=processer.process()
 
 # The agent processes the resume and generates a new resume
